0ee087e8b0019ca91b65668da2baa1d4bf74fd5a
ken <piq9117@yahoo.com>
2016-04-09 18:56:00 -0500

    Added reverseString.java this program will accept a string input and will immediately output a string

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# java
+# java <br/>
+Just 3 regular guys trying to learn some Java. <br/>
+
+<h4>Members:</h4>
+Ken - piq9117@yahoo.com
+John - <br/>
+Bobby - <br/>
---
+++ reverseString/src/reverseString/ReverseString.java
@@ -0,0 +1,13 @@
+package reverseString;
+import java.util.Scanner;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String strInput;
+		
+		strInput = input.nextLine();
+		System.out.println(strInput);
+	}
+
+}
