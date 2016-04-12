bccb61264e147f095ed770bf516d43cd4ac28751
booby <booya@Bobbys-MacBook-Pro.local>
2016-04-11 23:18:11 -0500

    suck that johnson

--- reverseString/src/reverseString/ReverseString.java
+++ reverseString/src/reverseString/ReverseString.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class ReverseString {
 	public static void main(String []args){
 		Scanner input = new Scanner(System.in);
-		//Scanner variable Declared Bobby likes Catherine's tae 
+		//Scanner variable declared, scanned johns ass and i can smell it a mile away scan complete 
 	}
 	//TODO Declare Scanner variable
 	//TODO Declare String variable
