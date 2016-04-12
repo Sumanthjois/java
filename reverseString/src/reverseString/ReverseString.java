413749483a135fb7b0201e48a205fd4903252fdd
ken <piq9117@yahoo.com>
2016-04-11 22:47:55 -0500

    added tasks

--- reverseString/src/reverseString/ReverseString.java
+++ reverseString/src/reverseString/ReverseString.java
@@ -3,4 +3,8 @@ import java.util.Scanner;
 
 public class ReverseString {
 	//TODO add main method
+	//TODO Declare Scanner variable
+	//TODO Declare String variable
+	//TODO Take input String input
+	//TODO Output the String input
 }
