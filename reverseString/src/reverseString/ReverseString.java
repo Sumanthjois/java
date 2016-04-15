aadd29b549f9ae429840700b6209334d35aad99e
ken <piq9117@yahoo.com>
2016-04-15 04:05:59 -0500

    done

--- reverseString/src/reverseString/ReverseString.java
+++ reverseString/src/reverseString/ReverseString.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 
 public class ReverseString {
 	public static void main (String[] args){
-		System.out.println(new StringBuilder("eat s'enirehtac eta ybbob").reverse().toString());
+		Scanner input = new Scanner(System.in);
+		String word, reversed;
+		
+		System.out.println("Enter a word and I will reverse it.");
+		word = input.nextLine();
+		reversed = new StringBuilder(word).reverse().toString();
+		System.out.println(reversed);
 	}
 }
