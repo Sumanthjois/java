fd6037828daa213fc15a0104a4ff5766103f17c1
ken <piq9117@yahoo.com>
2016-04-15 05:22:03 -0500

    readme

--- reverseString/src/reverseString/ReverseString.java
+++ reverseString/src/reverseString/ReverseString.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class ReverseString {
   //TODO main method
-  //TODO Declare Scanner, and String variables
+  //TODO Declare Scanner, and String variables(you will need 2)
   System.out.println("Give me a word and I will reverse it.");
-  //TODO use Scanner variable to capture the user input, and store it in String variable.
+  //TODO use Scanner variable to capture the user input by using the .nextLine() method, and store it in a String variable.
   //TODO reverse the String input using new StringBuilder(). Then, store it in a String variable
   //TODO output the reversed String.
 }
